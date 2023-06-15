be9b089e2de97ceaf76654e281b6ef53222ed0ac
Alexandre Magaud <alexandre.magaud@gmail.com>
2023-06-15 17:14:47 +0200

    feat: unit test with 2 successful successive jobs

--- libs/ledger-live-common/src/hw/deviceAccess.test.ts
+++ libs/ledger-live-common/src/hw/deviceAccess.test.ts
@@ -1,8 +1,8 @@
 import Transport from "@ledgerhq/hw-transport";
 import { withDevice } from "./deviceAccess";
-import { of } from "rxjs";
+import { from, of } from "rxjs";
 
-const deviceId = "test_device";
+jest.useFakeTimers();
 
 // Only mocks open
 jest.mock(".", () => {
@@ -15,8 +15,10 @@ jest.mock(".", () => {
   };
 });
 
+const deviceId = "test_device";
+
 describe("withDevice", () => {
-  describe("When there is only one job", () => {
+  describe("When there is only 1 job", () => {
     it("should provide a transport and run the given job", done => {
       const job = jest.fn().mockReturnValue(of("job test result"));
 
@@ -30,6 +32,46 @@ describe("withDevice", () => {
           }
         },
         error: e => done(`It should not have failed: ${JSON.stringify(e)}`),
+      });
+    });
+  });
+
+  describe("When there 1 job trying to run while another job is ongoing", () => {
+    describe("And the first job is successful", () => {
+      it("should run the 2 jobs successively", done => {
+        const job1Duration = 1000;
+        // Mocking rxjs timer is difficult -> using `setTimeout` instead of a rxjs `delay`
+        const job1 = jest.fn().mockReturnValue(
+          from(
+            new Promise(resolve => {
+              setTimeout(() => resolve("job 1 test result"), job1Duration);
+            }),
+          ),
+        );
+        const job2 = jest.fn().mockReturnValue(of("job 2 test result"));
+
+        // Registers first the job1
+        withDevice(deviceId)(job1).subscribe({
+          error: e => done(`It should not have failed: ${JSON.stringify(e)}`),
+        });
+
+        // And then job2, that should wait that job1 is finished
+        withDevice(deviceId)(job2).subscribe({
+          next: () => {
+            try {
+              // job1 should have been called before
+              expect(job1).toHaveBeenCalledWith(expect.any(Transport));
+              expect(job2).toHaveBeenCalledWith(expect.any(Transport));
+              done();
+            } catch (expectError) {
+              done(expectError);
+            }
+          },
+          error: e => done(`It should not have failed: ${JSON.stringify(e)}`),
+        });
+
+        // The timeout is equal to pollingPeriodMs by default
+        jest.advanceTimersByTime(job1Duration);
       });
     });
   });
